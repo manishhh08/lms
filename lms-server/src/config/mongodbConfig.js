917cd9510c473d61790518f7ff88336d27a8793a
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2025-08-05 13:11:40 +1000

    config update

--- lms-server/src/config/mongodbConfig.js
+++ lms-server/src/config/mongodbConfig.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 import { config } from "./config.js";
 
 export const mongodbConnection = async () => {
-  await mongoose.connect(
-    config.mongoOption.url,
-  );
+  await mongoose.connect(config.mongoOption.url);
 };
 
 export default mongodbConnection;
