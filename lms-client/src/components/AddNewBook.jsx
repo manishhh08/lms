147220a96838f9d1cd39643764dff599bc38689a
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2025-08-18 23:53:35 +1000

    new book form added

--- lms-client/src/components/AddNewBook.jsx
+++ lms-client/src/components/AddNewBook.jsx
@@ -5,50 +5,93 @@ import {
   Container,
   FloatingLabel,
   Form,
+  FormGroup,
   Row,
 } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { setBook } from "../features/books/bookSlice";
 
 const AddNewBook = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleOnChange = (e) => {
+    setAddBook({
+      ...addBook,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    // dispatch add new book
+
+    dispatch(setBook(addBook));
+
+    // back to book list
+    navigate("/");
+  };
+  const handleOnClick = () => {
+    navigate("/books");
+  };
+
+  const [addBook, setAddBook] = useState({
+    year: new Date().toISOString().split("T")[0],
+    bookTitle: "",
+    author: "",
+    genre: "",
+    imdb: "",
+    description: "",
+  });
   return (
     <>
       <Container>
         <Row>
           <Col>Add new books of your choice</Col>
         </Row>
+        <Button onClick={handleOnClick}>Go Back</Button>
       </Container>
 
-      <Container>
-        <Row>
-          <Col>
+      <Container className="pt-5">
+        <Form onSubmit={handleOnSubmit}>
+          <FormGroup>
             <FloatingLabel
               controlId="floatingInput"
               label="Book Title"
               className="mb-3"
+              onChange={handleOnChange}
+              value={addBook.bookTitle}
             >
-              <Form.Control type="text" placeholder="Book Title" />
+              <Form.Control type="text" placeholder="Book Title" required />
             </FloatingLabel>
             <FloatingLabel
               controlId="floatingInput"
               label="Author"
               className="mb-3"
+              onChange={handleOnChange}
+              value={addBook.author}
             >
-              <Form.Control type="text" placeholder="Author" />
+              <Form.Control type="text" placeholder="Author" required />
             </FloatingLabel>
             <FloatingLabel
               controlId="floatingInput"
               label="Date(YYYY-MM-DD)"
               className="mb-3"
+              onChange={handleOnChange}
+              value={addBook.date}
             >
-              <Form.Control type="date" placeholder="2023-09-22" />
+              <Form.Control type="date" placeholder="2023-09-22" required />
             </FloatingLabel>
-            {/* <FloatingLabel
-              controlId="floatingInput"
-              label="ISBN"
+
+            <Form.Select
+              aria-label="Default select example"
               className="mb-3"
+              onChange={handleOnChange}
+              value={addBook.imdb}
             >
-              <Form.Control type="number" placeholder="5" />
-            </FloatingLabel> */}
-            <Form.Select aria-label="Default select example" className="mb-3">
               {/* <option>IMDB rating</option> */}
               <option value="1" selected>
                 1
@@ -60,26 +103,23 @@ const AddNewBook = () => {
               controlId="floatingInput"
               label="Description"
               className="mb-3"
+              onChange={handleOnChange}
+              value={addBook.description}
             >
-              <Form.Control type="text" placeholder="Description" />
+              <Form.Control type="text" placeholder="Description" required />
             </FloatingLabel>
             <FloatingLabel
               controlId="floatingInput"
               label="Genre"
               className="mb-3"
+              onChange={handleOnChange}
+              value={addBook.genre}
             >
-              <Form.Control type="text" placeholder="Genre" />
+              <Form.Control type="text" placeholder="Genre" required />
             </FloatingLabel>
-          </Col>
-        </Row>
-        <Button
-          onClick={() => {
-            alert("form submitted");
-          }}
-        >
-          {" "}
-          Submit
-        </Button>
+          </FormGroup>
+        </Form>
+        <Button type="submit">Submit</Button>
       </Container>
     </>
   );
--- lms-client/src/pages/Book.jsx
+++ lms-client/src/pages/Book.jsx
@@ -7,8 +7,18 @@ import {
   Row,
   Table,
 } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Book = () => {
+  const navigate = useNavigate();
+
+  const handleOnClick = () => {
+    navigate("/add-book");
+  };
+
+  const { book } = useSelector((store) => store.bookStore);
+
   return (
     <Container>
       <Row className="mt-3">
@@ -16,113 +26,65 @@ const Book = () => {
           <h5>Book Section</h5>
         </Col>
       </Row>
-      <Button
-        onClick={() => {
-          alert("new books added here");
-        }}
-      >
-        Add New Books
-      </Button>
+      <Button onClick={handleOnClick}>Add New Books</Button>
 
       <Row className="pt-5">
         <Table variant="dark">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Date</th>
-              <th>Book Name</th>
-              <th>Author</th>
-              <th>Status</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>2023-08-10</td>
-              <td>Dented</td>
-              <td>Code</td>
-              <td>Available</td>
-              <td>
-                <ButtonGroup className="gap-2">
-                  <Button
-                    className="bg-danger"
-                    onClick={() => {
-                      alert("Delete button clicked");
-                    }}
-                  >
-                    Delete
-                  </Button>
+          <>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Year</th>
+                <th>Book Title</th>
+                <th>Author</th>
+                <th>Genre</th>
+                <th>Status</th>
+                <th>Action</th>
+              </tr>
+            </thead>
 
-                  <Button
-                    className="bg-warning"
-                    onClick={() => {
-                      alert("Update button clicked");
-                    }}
-                  >
-                    Update
-                  </Button>
-                </ButtonGroup>
-              </td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>2023-08-10</td>
-              <td>Dented</td>
-              <td>Code</td>
-              <td>Available</td>
-              <td>
-                <ButtonGroup className="gap-2">
-                  <Button
-                    className="bg-danger"
-                    onClick={() => {
-                      alert("Delete button clicked");
-                    }}
-                  >
-                    Delete
-                  </Button>
-
-                  <Button
-                    className="bg-warning"
-                    onClick={() => {
-                      alert("Update button clicked");
-                    }}
-                  >
-                    Update
-                  </Button>
-                </ButtonGroup>
-              </td>
-            </tr>
-
-            <tr>
-              <td>1</td>
-              <td>2023-08-10</td>
-              <td>Dented</td>
-              <td>Code</td>
-              <td>Available</td>
-              <td>
-                <ButtonGroup className="gap-2">
-                  <Button
-                    className="bg-danger"
-                    onClick={() => {
-                      alert("Delete button clicked");
-                    }}
-                  >
-                    Delete
-                  </Button>
-
-                  <Button
-                    className="bg-warning"
-                    onClick={() => {
-                      alert("Update button clicked");
-                    }}
-                  >
-                    Update
-                  </Button>
-                </ButtonGroup>
-              </td>
-            </tr>
-          </tbody>
+            <tbody>
+              {book.length === 0 ? (
+                <tr>
+                  <td colSpan={7} className="text-center">
+                    No Books Added
+                  </td>
+                </tr>
+              ) : (
+                book.map((b, index) => {
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{b.year}</td>
+                    <td>{b.bookTitle}</td>
+                    <td>{b.author}</td>
+                    <td>{b.genre}</td>
+                    {/* <td>{b.imdb}</td>
+                    <td>{b.description}</td> */}
+                    <td>
+                      <ButtonGroup className="gap-2">
+                        <Button
+                          className="btn btn-danger"
+                          onClick={() => {
+                            alert("Delete");
+                          }}
+                        >
+                          Delete
+                        </Button>
+                        <Button
+                          className="btn btn-warning"
+                          onClick={() => {
+                            alert("Update");
+                          }}
+                        >
+                          Update
+                        </Button>
+                      </ButtonGroup>
+                    </td>
+                  </tr>;
+                })
+              )}
+            </tbody>
+          </>
         </Table>
       </Row>
     </Container>
